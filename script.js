9aca29186b0e5340b28799b382982f15ca080991
Pankaj Das <kumardas67@gmail.com>
2023-10-28 14:11:08 +0530

    Project Completed

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculateLoan(){
+    loanAmountValue = document.getElementById('loan-amount').value
+    interestRateValue = document.getElementById('interest-rate').value
+    monthsToPay = document.getElementById('months-to-pay').value
+
+    interest = (loanAmountValue * (interestRateValue * 0.01)) / monthsToPay
+
+    monthlyPayment = (loanAmountValue / monthsToPay + interest).toFixed(2)
+
+    document.getElementById('payment').innerHTML = `Monthly Payment: ${monthlyPayment}`
+    
+}
